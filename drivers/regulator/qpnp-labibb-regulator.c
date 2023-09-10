c4f3294d578084303d96a6d8ecadfaa72c4c21ce
Haris Wu <bymark256@gmail.com>
2023-09-11 06:06:35 +0800

    regulator: qpnp-labibb: update QPNP LAB/IBB regulator from msm-4.9
    This avoids modifying the devicetree.
    
    Signed-off-by: Haris Wu <bymark256@gmail.com>

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2017, 2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2466,9 +2466,6 @@ static int qpnp_lab_regulator_enable(struct regulator_dev *rdev)
 	int rc;
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
 
-	if (labibb->secure_mode)
-		return 0;
-
 	if (labibb->sc_detected) {
 		pr_info("Short circuit detected: disabled LAB/IBB rails\n");
 		return 0;
@@ -2505,9 +2502,6 @@ static int qpnp_lab_regulator_disable(struct regulator_dev *rdev)
 	int rc;
 	u8 val;
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
-
-	if (labibb->secure_mode)
-		return 0;
 
 	if (labibb->lab_vreg.vreg_enabled && !labibb->swire_control) {
 
@@ -2702,7 +2696,7 @@ static int qpnp_lab_regulator_set_voltage(struct regulator_dev *rdev,
 	u8 val;
 	struct qpnp_labibb *labibb = rdev_get_drvdata(rdev);
 
-	if (labibb->swire_control || labibb->secure_mode)
+	if (labibb->swire_control)
 		return 0;
 
 	if (min_uV < labibb->lab_vreg.min_volt) {
@@ -3451,7 +3445,7 @@ static int qpnp_ibb_dt_init(struct qpnp_labibb *labibb,
 		}
 		rc = labibb->ibb_ver_ops->set_clk_div(labibb, val);
 		if (rc < 0) {
-			pr_err("write register %x failed rc = %d\n",
+			pr_err("qpnp_ibb_dt_init write register %x failed rc = %d\n",
 				REG_IBB_CLK_DIV, rc);
 			return rc;
 		}
@@ -3512,7 +3506,7 @@ static int qpnp_ibb_dt_init(struct qpnp_labibb *labibb,
 					&val,
 					1);
 		if (rc < 0) {
-			pr_err("write register %x failed rc = %d\n",
+			pr_err("qpnp_ibb_dt_init write register %x failed rc = %d\n",
 				REG_IBB_RING_SUPPRESSION_CTL, rc);
 			return rc;
 		}
@@ -3521,13 +3515,13 @@ static int qpnp_ibb_dt_init(struct qpnp_labibb *labibb,
 	if (of_property_read_bool(of_node, "qcom,qpnp-ibb-ps-enable")) {
 		rc = qpnp_ibb_ps_config(labibb, true);
 		if (rc < 0) {
-			pr_err("PS enable failed rc=%d\n", rc);
+			pr_err("qpnp_ibb_dt_init PS enable failed rc=%d\n", rc);
 			return rc;
 		}
 	} else {
 		rc = qpnp_ibb_ps_config(labibb, false);
 		if (rc < 0) {
-			pr_err("PS disable failed rc=%d\n", rc);
+			pr_err("qpnp_ibb_dt_init PS disable failed rc=%d\n", rc);
 			return rc;
 		}
 	}
@@ -3543,7 +3537,8 @@ static int qpnp_ibb_dt_init(struct qpnp_labibb *labibb,
 		rc = labibb->ibb_ver_ops->smart_ps_config(labibb, true,
 					labibb->ibb_vreg.num_swire_trans, tmp);
 		if (rc < 0) {
-			pr_err("smart PS enable failed rc=%d\n", rc);
+			pr_err("qpnp_ibb_dt_init smart PS enable failed rc=%d\n",
+					 rc);
 			return rc;
 		}
 
@@ -3579,9 +3574,6 @@ static int qpnp_ibb_regulator_enable(struct regulator_dev *rdev)
 	int rc = 0;
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
 
-	if (labibb->secure_mode)
-		return 0;
-
 	if (labibb->sc_detected) {
 		pr_info("Short circuit detected: disabled LAB/IBB rails\n");
 		return 0;
@@ -3606,9 +3598,6 @@ static int qpnp_ibb_regulator_disable(struct regulator_dev *rdev)
 {
 	int rc;
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
-
-	if (labibb->secure_mode)
-		return 0;
 
 	if (labibb->ibb_vreg.vreg_enabled && !labibb->swire_control) {
 
@@ -3643,7 +3632,7 @@ static int qpnp_ibb_regulator_set_voltage(struct regulator_dev *rdev,
 
 	struct qpnp_labibb *labibb = rdev_get_drvdata(rdev);
 
-	if (labibb->swire_control || labibb->secure_mode)
+	if (labibb->swire_control)
 		return 0;
 
 	rc = labibb->ibb_ver_ops->set_voltage(labibb, min_uV, max_uV);
@@ -3872,8 +3861,6 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 	}
 
 	if (labibb->ibb_vreg.ibb_sc_irq != -EINVAL) {
-		irq_set_status_flags(labibb->ibb_vreg.ibb_sc_irq,
-				     IRQ_DISABLE_UNLAZY);
 		rc = devm_request_threaded_irq(labibb->dev,
 				labibb->ibb_vreg.ibb_sc_irq, NULL,
 				labibb_sc_err_handler,
@@ -4034,52 +4021,6 @@ static int qpnp_labibb_check_ttw_supported(struct qpnp_labibb *labibb)
 	}
 	return rc;
 }
-
-static ssize_t secure_mode_store(struct class *c,
-					struct class_attribute *attr,
-					const char *buf, size_t count)
-{
-	struct qpnp_labibb *labibb = container_of(c, struct qpnp_labibb,
-						  labibb_class);
-	int val, rc;
-
-	rc = kstrtouint(buf, 0, &val);
-	if (rc < 0)
-		return rc;
-
-	if (val != 0 && val != 1)
-		return count;
-
-	/* Disable irqs */
-	if (val == 1 && !labibb->secure_mode) {
-		if (labibb->lab_vreg.lab_vreg_ok_irq > 0)
-			disable_irq(labibb->lab_vreg.lab_vreg_ok_irq);
-		if (labibb->lab_vreg.lab_sc_irq > 0)
-			disable_irq(labibb->lab_vreg.lab_sc_irq);
-		if (labibb->ibb_vreg.ibb_sc_irq > 0)
-			disable_irq(labibb->ibb_vreg.ibb_sc_irq);
-		labibb->secure_mode = true;
-	} else if (val == 0 && labibb->secure_mode) {
-		if (labibb->lab_vreg.lab_vreg_ok_irq > 0)
-			enable_irq(labibb->lab_vreg.lab_vreg_ok_irq);
-		if (labibb->lab_vreg.lab_sc_irq > 0)
-			enable_irq(labibb->lab_vreg.lab_sc_irq);
-		if (labibb->ibb_vreg.ibb_sc_irq > 0)
-			enable_irq(labibb->ibb_vreg.ibb_sc_irq);
-		labibb->secure_mode = false;
-	}
-
-	return count;
-}
-
-static CLASS_ATTR_WO(secure_mode);
-
-static struct attribute *labibb_attrs[] = {
-	&class_attr_secure_mode.attr,
-	NULL,
-};
-ATTRIBUTE_GROUPS(labibb);
-
 
 static int qpnp_labibb_regulator_probe(struct platform_device *pdev)
 {
@@ -4273,16 +4214,6 @@ static int qpnp_labibb_regulator_probe(struct platform_device *pdev)
 			CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	labibb->sc_err_check_timer.function = labibb_check_sc_err_count;
 	dev_set_drvdata(&pdev->dev, labibb);
-
-	labibb->labibb_class.name = "lcd_bias";
-	labibb->labibb_class.owner = THIS_MODULE;
-	labibb->labibb_class.class_groups = labibb_groups;
-
-	rc = class_register(&labibb->labibb_class);
-	if (rc < 0) {
-		pr_err("Failed to register labibb class rc=%d\n", rc);
-		return rc;
-	}
 
 	pr_info("LAB/IBB registered successfully, lab_vreg enable=%d ibb_vreg enable=%d swire_control=%d\n",
 						labibb->lab_vreg.vreg_enabled,
