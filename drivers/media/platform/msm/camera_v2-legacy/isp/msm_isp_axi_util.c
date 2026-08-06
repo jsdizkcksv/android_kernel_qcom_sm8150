DooPrjkt/android_kernel_qcom_msm8953

drivers: msm: camera_v2-legacy: add read fusion id for xiaomi mido

drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_axi_util.c
@@ -3262,7 +3262,11 @@ int msm_isp_cfg_axi_stream(struct vfe_device *vfe_dev, void *arg)
 	int rc = 0, ret;
 	struct msm_vfe_axi_stream_cfg_cmd *stream_cfg_cmd = arg;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev->axi_data;
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+	enum msm_isp_camif_update_state camif_update = NO_UPDATE;
+#else
 	enum msm_isp_camif_update_state camif_update;
+#endif
 	int halt = 0;
 
 	rc = msm_isp_axi_check_stream_state(vfe_dev, stream_cfg_cmd);

drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
@@ -676,6 +676,129 @@ static void msm_sensor_fill_sensor_info(struct msm_sensor_ctrl_t *s_ctrl,
 	strlcpy(entity_name, s_ctrl->msm_sd.sd.entity.name, MAX_SENSOR_NAME);
 }
 
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+extern int main_module_id;
+extern int sub_module_id;
+static const char *module_info[] = {
+	"Unkonw",
+	"Sunny",
+	"Huaquan",
+	"Fushikang",
+	"Guangzhen",
+	"Daling",
+	"Xinli",
+	"O-film",
+	"Boyi",
+	"Sanglaishi",
+	"Qunhui",
+	"Q-Tech",
+	"Unknow",
+	"Unknow",
+	"Unknow",
+	"Unknow",
+};
+
+static uint16_t fusion_read_id_s5k3l8(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t value1, value3, value5;
+
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x0100, 2);
+	mdelay(10);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a02, 0x0000, 2);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x0100, 2);
+	mdelay(10);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a24,  &value1, 2);
+	CDBG(" s5k3l8 fusion_sensor_readreg value1 =%d\n", value1);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a26,  &value3, 2);
+	CDBG(" s5k3l8 fusion_sensor_readreg value3 =%d\n", value3);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a28,  &value5, 1);
+	CDBG(" s5k3l8 fusion_sensor_readreg value5 =%d\n", value5);
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_ov5675(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[15] = {0};
+	uint16_t *p = NULL;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x01, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x5001, 0x02, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d84, 0xC0, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d88, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d89, 0x00, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8a, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8b, 0x0f, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d81, 0x01, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 15; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x7000+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_s5k5e8(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[8] = {0};
+	uint16_t *p = NULL;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x04&0x00ff, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a02, 0x00&0x00ff, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x01&0x00ff, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 8; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a04+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_ov13855(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[16] = {0};
+	uint16_t *p = NULL;
+	uint16_t temp1 = 0x0;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x01, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x5000, &temp1, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x5000, (0x00 & 0x10) | (temp1 & (~0x10)), 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d84, 0xC0, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d88, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d89, 0x00, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8a, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8b, 0x0f, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d81, 0x01, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 15; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x7000+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+#endif
+
 /* static function definition */
 int32_t msm_sensor_driver_probe(void *setting,
 	struct msm_sensor_info_t *probed_info, char *entity_name)
@@ -969,6 +1092,20 @@ CSID_TG:
 		pr_err("%s power up failed", slave_info->sensor_name);
 		goto free_camera_info;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+	if (!strcmp(slave_info->sensor_name, "ov13855_sunny")) {
+		fusion_read_id_ov13855(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "s5k3l8_ofilm")) {
+		fusion_read_id_s5k3l8(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "ov5675_ofilm")) {
+		fusion_read_id_ov5675(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "s5k5e8_sunny")) {
+		fusion_read_id_s5k5e8(s_ctrl);
+	} else {
+		printk("read fusion id fail\n");
+	}
+#endif
 
 	pr_err("%s probe succeeded", slave_info->sensor_name);
 

jsdizkcksv/android_kernel_qcom_sm8150

drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_axi_util.c
@@ -3262,7 +3262,11 @@ int msm_isp_cfg_axi_stream(struct vfe_device *vfe_dev, void *arg)
 	int rc = 0, ret;
 	struct msm_vfe_axi_stream_cfg_cmd *stream_cfg_cmd = arg;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev->axi_data;
+#ifdef CONFIG_MACH_XIAOMI_C6
+	enum msm_isp_camif_update_state camif_update = NO_UPDATE;
+#else
 	enum msm_isp_camif_update_state camif_update;
+#endif
 	int halt = 0;
 
 	rc = msm_isp_axi_check_stream_state(vfe_dev, stream_cfg_cmd);

drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
@@ -676,6 +676,129 @@ static void msm_sensor_fill_sensor_info(struct msm_sensor_ctrl_t *s_ctrl,
 	strlcpy(entity_name, s_ctrl->msm_sd.sd.entity.name, MAX_SENSOR_NAME);
 }
 
+#ifdef CONFIG_MACH_XIAOMI_C6
+extern int main_module_id;
+extern int sub_module_id;
+static const char *module_info[] = {
+	"Unkonw",
+	"Sunny",
+	"Huaquan",
+	"Fushikang",
+	"Guangzhen",
+	"Daling",
+	"Xinli",
+	"O-film",
+	"Boyi",
+	"Sanglaishi",
+	"Qunhui",
+	"Q-Tech",
+	"Unknow",
+	"Unknow",
+	"Unknow",
+	"Unknow",
+};
+
+static uint16_t fusion_read_id_s5k3l8(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t value1, value3, value5;
+
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x0100, 2);
+	mdelay(10);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a02, 0x0000, 2);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x0100, 2);
+	mdelay(10);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a24,  &value1, 2);
+	CDBG(" s5k3l8 fusion_sensor_readreg value1 =%d\n", value1);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a26,  &value3, 2);
+	CDBG(" s5k3l8 fusion_sensor_readreg value3 =%d\n", value3);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a28,  &value5, 1);
+	CDBG(" s5k3l8 fusion_sensor_readreg value5 =%d\n", value5);
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_ov5675(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[15] = {0};
+	uint16_t *p = NULL;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x01, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x5001, 0x02, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d84, 0xC0, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d88, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d89, 0x00, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8a, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8b, 0x0f, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d81, 0x01, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 15; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x7000+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_s5k5e8(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[8] = {0};
+	uint16_t *p = NULL;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x04&0x00ff, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a02, 0x00&0x00ff, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0a00, 0x01&0x00ff, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 8; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x0a04+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+
+static uint16_t fusion_read_id_ov13855(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	uint16_t data[16] = {0};
+	uint16_t *p = NULL;
+	uint16_t temp1 = 0x0;
+	uint8_t i;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x0100, 0x01, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x5000, &temp1, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x5000, (0x00 & 0x10) | (temp1 & (~0x10)), 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d84, 0xC0, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d88, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d89, 0x00, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8a, 0x70, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d8b, 0x0f, 1);
+	sensor_i2c_client->i2c_func_tbl->i2c_write(sensor_i2c_client, 0x3d81, 0x01, 1);
+	mdelay(10);
+
+	p = data;
+	for (i = 0; i < 15; i++) {
+		sensor_i2c_client->i2c_func_tbl->i2c_read(sensor_i2c_client, 0x7000+i, p+i, 1);
+		CDBG("data[%d]=%x\n", i, data[i]);
+	}
+
+	return 0;
+}
+#endif
+
 /* static function definition */
 int32_t msm_sensor_driver_probe(void *setting,
 	struct msm_sensor_info_t *probed_info, char *entity_name)
@@ -969,6 +1092,20 @@ CSID_TG:
 		pr_err("%s power up failed", slave_info->sensor_name);
 		goto free_camera_info;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_C6
+	if (!strcmp(slave_info->sensor_name, "ov13855_sunny")) {
+		fusion_read_id_ov13855(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "s5k3l8_ofilm")) {
+		fusion_read_id_s5k3l8(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "ov5675_ofilm")) {
+		fusion_read_id_ov5675(s_ctrl);
+	} else if (!strcmp(slave_info->sensor_name, "s5k5e8_sunny")) {
+		fusion_read_id_s5k5e8(s_ctrl);
+	} else {
+		printk("read fusion id fail\n");
+	}
+#endif
 
 	pr_err("%s probe succeeded", slave_info->sensor_name);
 